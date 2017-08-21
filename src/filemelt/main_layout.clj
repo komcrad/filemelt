28005323692285181527d12a22f2e2ddc7979e12
Konrad Tallman <konradt95@gmail.com>
2017-08-21 10:29:37 -0500

    Adds device width scale

--- src/filemelt/main_layout.clj
+++ src/filemelt/main_layout.clj
@@ -7,7 +7,7 @@
           [:head
 		   (slurp (clojure.java.io/resource "public/favicon.html"))
            [:link {:rel "stylesheet" :type "text/css" :href "/styles/main.css"}]]
-		   [:script {:src "/js/dropzone.js"}]
+           [:meta {:name "viewport" :content "device-width, initial-scale=1.0"}]
           [:body
            [:nav
             [:a {:href "/"} "Home"]]
